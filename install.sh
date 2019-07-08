fa567d427533145310c5c616bb20ece09da5bad7
iamchuckss <s2011297@gmail.com>
2019-07-08 17:38:52 -0500

    add installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# install - install a program, script, or datafile
+
+pipenv install --ignore-pipfile
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+ln -s "$DIR"/create-project.sh /usr/local/bin/create-project
+
+echo "Installed project creation automation tool successfully!"
+echo "To start using, add your github credentials and project directory in the config.py configuration file"
+echo "Then, run create-project <project-name> to create a new project!"
