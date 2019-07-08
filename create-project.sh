772f767080607cfea3036325c6e917af688e0641
iamchuckss <s2011297@gmail.com>
2019-07-08 16:58:48 -0500

    update script to run in python3

--- create-project.sh
+++ create-project.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+my_dir=$(dirname "$(readlink "$0")")
+cd "$my_dir"
 USER="$(python -c 'import config;print(config.username)')" 
 DIRECTORY="$(python -c 'import config;print(config.project_directory)')/$1"
-python create.py $1
+python3 create.py $1
 if [ -d $DIRECTORY ]; then
     cd "$DIRECTORY"
     git init
