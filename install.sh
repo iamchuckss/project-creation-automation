62d56ec63a6900e79975fee2218638517eec1adc
iamchuckss <s2011297@gmail.com>
2019-07-09 18:03:28 -0500

    fix installer to overwrite old installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 pipenv install --ignore-pipfile
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-ln -s "$DIR"/create-project.sh /usr/local/bin/create-project
+ln -sf "$DIR"/create-project.sh /usr/local/bin/create-project
 
 echo "Installed project creation automation tool successfully!"
 echo "To start using, add your github credentials and project directory in the config.py configuration file"
