3121ceebcf4197ac0e3f590e98ffc2068c2ae94c
iamchuckss <s2011297@gmail.com>
2019-07-09 18:16:01 -0500

    fix create-project to handle no args error

--- create-project.sh
+++ create-project.sh
@@ -4,14 +4,21 @@ my_dir=$(dirname "$(readlink "$0")")
 cd "$my_dir"
 USER="$(python -c 'import config;print(config.username)')" 
 DIRECTORY="$(python -c 'import config;print(config.project_directory)')/$1"
-python3 create.py $1
-if [ -d $DIRECTORY ]; then
-    cd "$DIRECTORY"
-    git init
-    git remote add origin git@github.com:"$USER"/$1.git
-    touch README.md
-    git add -A
-    git commit -m "Initial commit"
-    git push -u origin master
-    code .
-fi
+if [ -n "$1" ]; then 
+    python3 create.py $1
+    if [ -d $DIRECTORY ]; then
+        cd "$DIRECTORY"
+        git init
+        git remote add origin git@github.com:"$USER"/$1.git
+        touch README.md
+        git add -A
+        git commit -m "Initial commit"
+        git push -u origin master
+        code .
+    else
+        echo "ERROR: Unable to create new directory..."
+    fi 
+else
+    echo "Usage: create-project <project-name>"
+fi 
+
